bd765409ece0c9d860e2d141c3287af5a284cb99
SylviaRatemo <ratemosylvia@gmail.com>
2023-05-13 03:42:14 -0700

    user module

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Class user that inherits BaseModel"""
+from models import BaseModel
+
+class User:
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
