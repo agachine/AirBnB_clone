b7fe065c3948011ee2d99793397b45a9f7391c59
SylviaRatemo <ratemosylvia@gmail.com>
2023-05-20 06:32:16 -0700

    Errors

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization of the BaseModel"""
         if kwargs:
-            for k, v in kwargs.items():
-                if k == 'created_at' or k == 'updated_at':
-                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
-                if k != '__class__':
-                    setattr(self, k, v)
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
