ece7222310d345ce4d004c49554268073b99017b
SylviaRatemo <ratemosylvia@gmail.com>
2023-05-20 04:05:17 -0700

    Save error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,11 +19,9 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
-        obj_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            obj_dict[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(obj_dict, file)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
